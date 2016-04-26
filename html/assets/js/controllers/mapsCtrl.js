daca1d5f4851f2dedd210f2c66cf45d2648b9a71
Ghazal <ghazal.karami@unimelb.edu.au>
2016-04-26 16:01:00 +1000

    Display a sample geojson aurin dataset  on the google map using ngmap

--- html/assets/js/controllers/mapsCtrl.js
+++ html/assets/js/controllers/mapsCtrl.js
@@ -1,5 +1,24 @@
 'use strict';
-/** 
-  * controllers for GoogleMap 
-  * AngularJS Directive
-*/
+/**
+ * controllers for GoogleMap
+ * AngularJS Directive
+ */
+
+
+app.controller('mapsCtrl', ['$scope', 'NgMap', function ($scope, NgMap) {
+  var vm = this;
+
+  NgMap.getMap().then(function (map) {
+    //    [144.8889,-37.8917,145.0453,-37.7325]
+
+    vm.map = map;
+    console.log(map.getBounds());
+  });
+
+  vm.onClick = function (event) {
+    vm.geoType = event.feature.getGeometry().getType();
+    vm.geoArray = event.feature.getGeometry().getArray();
+    vm.geoName = event.feature.R.REGION_NAME;
+    console.dir('geoArray', event.feature.getGeometry().getArray());
+  };
+}]);
