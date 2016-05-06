8aa2aebb632d8e41bbc44aec8dc03804ea844d25
Ghazal <ghazal.karami@unimelb.edu.au>
2016-05-06 12:22:52 +1000

    Update mapsCtrl.js

--- html/assets/js/controllers/mapsCtrl.js
+++ html/assets/js/controllers/mapsCtrl.js
@@ -7,18 +7,30 @@
 
 app.controller('mapsCtrl', ['$scope', 'NgMap', function ($scope, NgMap) {
   var vm = this;
+  var infowindow = new google.maps.InfoWindow({
+    content: 'hi sfdhkd'
+  });
 
   NgMap.getMap().then(function (map) {
-    //    [144.8889,-37.8917,145.0453,-37.7325]
 
     vm.map = map;
     console.log(map.getBounds());
-  });
 
-  vm.onClick = function (event) {
-    vm.geoType = event.feature.getGeometry().getType();
-    vm.geoArray = event.feature.getGeometry().getArray();
-    vm.geoName = event.feature.R.REGION_NAME;
-    console.dir('geoArray', event.feature.getGeometry().getArray());
-  };
+    vm.map.data.addListener('click', function(event) {
+      var myHTML = event.feature.getProperty('REGION_NAME');
+      infowindow.setContent(myHTML);
+      infowindow.setPosition(vm.map.getCenter());
+      infowindow.setOptions({pixelOffset: 10});
+      infowindow.open(vm.map);
+    });
+
+//    vm.onClick = function (event) {
+//      vm.geoType = event.feature.getGeometry().getType();
+//      vm.geoArray = event.feature.getGeometry().getArray();
+//      vm.geoName = event.feature.R.REGION_NAME;
+//      console.dir('geoArray', event.feature.getGeometry().getArray());
+//    };
+
+  });
 }]);
+
